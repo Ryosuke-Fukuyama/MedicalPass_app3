2ac5dba6f9d529cf6df07199e0149449987e6f5f
ryosuke fukuyama <ryosukemakaritoru425@gmail.com>
2021-07-31 00:48:10 +0900

    [update] #3 enum

--- app/models/guide_label.rb
+++ app/models/guide_label.rb
@@ -2,4 +2,6 @@ class GuideLabel < ApplicationRecord
   belongs_to :health_interview
 
   validates :status, presence: true
+
+  enum status: { initial: 0, calling: 1, done: 2, pending: 3, noshow: 4, complete: 5 }
 end
