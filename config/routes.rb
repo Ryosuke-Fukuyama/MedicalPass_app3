61138b95668c04fd002231be1a6c9a3d085dc7cd
ryosuke fukuyama <ryosukemakaritoru425@gmail.com>
2021-07-31 01:39:34 +0900

    [update] #3 root

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
-  get 'health_interviews/index'
-  get 'health_interviews/new'
-  get 'health_interviews/show'
-  get 'health_interviews/edit'
-  get 'health_interviews/_each_list'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # root 'tops#index'
+  root 'health_interviews#index'
+  resources :health_interviews
 end
