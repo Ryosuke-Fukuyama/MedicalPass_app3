bf03a5f5001e92141e32f4aa0b2e6ce59f30bdf9
ryosuke fukuyama <ryosukemakaritoru425@gmail.com>
2021-08-09 10:10:32 +0900

    [fix] #3 package.jsonエラー対応

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,9 @@ Rails.application.routes.draw do
   root 'health_interviews#index'
   resources :health_interviews
   # post 'health_interviews/index'
-  namespace :api, format: 'json' do
-    namespace :v1 do
-      resources :health_interviews, only: %i[index update]
-    end
-  end
+  # namespace :api, format: 'json' do
+  #   namespace :v1 do
+  #     resources :health_interviews, only: %i[index update]
+  #   end
+  # end
 end
