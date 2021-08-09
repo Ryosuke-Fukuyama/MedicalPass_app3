f54c3bff6747fa07c63bfaaf759b66f425810592
ryosuke fukuyama <ryosukemakaritoru425@gmail.com>
2021-08-09 12:25:26 +0900

    [fix] #3 vue置換

--- app/javascript/status_update.vue
+++ app/javascript/status_update.vue
@@ -30,80 +30,25 @@ export default {
       message: 'ここに「Ajax」に関するメッセージが表示。'
     }
   },
-  //  Ajax通信(送信用)
   mounted() {
     axios.get('/health_interviews/index.json')
       .then(response => (this.guide_labels = response.data))
   },
   methods: {
-  //   run_ajax: function(method, url, data) {
-  //     fetch(url,
-  //          {
-  //            method: method,
-  //            body: JSON.stringify(data),
-  //            headers:{
-  //             'Content-Type': 'application/json',
-  //             'X-CSRF-TOKEN': document.querySelector('meta[name="csrf-token"]').getAttribute('status')
-  //            }
-  //          })
-  //       .then(res => res.json())
-  //       .then(
-  //         (result) => {
-  //           this.status = "サーバーからのメッセージ(" +
-  //                         this.formatConversion(new Date())  + ") ：" + result.registration;
-  //           // 新規登録時のみIDなどが返却される
-  //           if(result.id){
-  //             // 失敗
-  //             if(result.id == "error"){
-  //               // エラー制御は行っていないので各自で。
-
-  //             // 成功
-  //             }else{
-  //               // 先頭にアイテムを追加する
-  //               this.items.unshift({id: result.id,
-  //                                   name: result.name,
-  //                                   comment: result.comment,
-  //                                   updated_at: result.updated_at}
-  //                                  );
-  //               this.mode.unshift(false);
-  //             }
-  //           // 更新/削除
-  //           }else{
-  //             // エラー制御は行っていないので各自で。
-  //           }
-
-  //         },
-  //         (error) => {
-  //           this.status = error.message;
-  //         }
-  //       )
-  //       .catch((error) => {
-  //            this.status = error.message;
-  //         }
-  //       );
-  //   },
-
-    //  データの更新
     handleUpdate: function (index, id, event) {
       const form_data = new FormData(event.target);
 
-      const txt_name = form_data.get('txt_name');
-      const txt_comment = form_data.get('txt_comment');
+      const status = form_data.get('status');
 
       if (
-          (txt_name && txt_comment) &&
-          (!(this.items[index].name == txt_name &&
-             this.items[index].comment == txt_comment))
+          (!(this.items[index].status == status))
          ){
 
-        this.$root.$set(this.items[index], "name", txt_name);
-        this.$root.$set(this.items[index], "comment", txt_comment);
+        this.$root.$set(this.items[index], "status", status);
         this.$root.$set(this.items[index], "updated_at", new Date());
-
-        // Ajax
         this.run_ajax("PUT",
-                      "http://localhost:3000/vue_crud_data/"  + id ,
-                      {datum: {name: txt_name, comment: txt_comment}}
+                      "http://localhost:3000/health_interviews/" ,
+                      {guide_label: {status: status}}
                      );
       }
     },
