8b1d87eb191819950b269fbe5def7dded3775219
ryosuke fukuyama <ryosukemakaritoru425@gmail.com>
2021-08-14 10:30:26 +0900

    [fix] #11 search_from_for アトリビュートカラムの指定方法

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,11 +2,11 @@ class ApplicationController < ActionController::Base
   # protect_from_forgery with: :exception
 
   def admin_required
-    redirect_back fallback_location: { action: "index" } unless staff_signed_in? && current_staff.admin? && current_staff.hospital_id == @hospital.id
+    redirect_back fallback_location: { action: "index" } unless staff_signed_in? && current_staff.admin? && current_staff.hospital_id # == @hospital.id
   end
 
   def staff_required
-    redirect_back fallback_location: { action: "index" } unless staff_signed_in? && current_staff.hospital_id == @hospital.id
+    redirect_back fallback_location: { action: "index" } unless staff_signed_in? && current_staff.hospital_id # == @hospital.id
   end
 
   def patient_required
